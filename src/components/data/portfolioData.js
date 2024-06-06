b0c8c9ce0e52c03fbb75f3c77616d7e9c7f00545
Gian <165049398+gtricerri@users.noreply.github.com>
2024-06-06 18:26:25 -0400

    first commit

--- src/components/data/portfolioData.js
+++ src/components/data/portfolioData.js
@@ -1,5 +1,5 @@
 /**
- * @SofiDev Esto es JSDOC, si consideras que puede ser complicado solo borralo, es un comentario, no afectará en nada
+ * @JuanSantil Esto es JSDOC, si consideras que puede ser complicado solo borralo, es un comentario, no afectará en nada
  * @typedef PortafolioData
  * @property {string} imgSrc Url de la imagen
  * @property {string} title Titulo de la tarjeta
@@ -7,33 +7,36 @@
  * @property {string} descripcion La descripcion de la tarjeta
  * @property {string} demoURL Url de una pagina de demostración
  * @property {string} repoURL Url del repositorio, ej: https://github.com/usuario/repo
+ * @property {string} caseURL Url del para info del caso
  * @property {string} anim La animación que se ejecutará cuando se cargue la tarjeta, ej: fade-up, fade-right, fade-left, fade-down
  * @property {number} averageBrightness Cuanto brillo tendrá el color de fondo de la tarjeta, ej: 0.1
  */
 
 /**
- * @SofiDev Esto es JSDOC, si consideras que puede ser complicado solo borralo, es un comentario, no afectará en nada
+ * @JuanSantil Esto es JSDOC, si consideras que puede ser complicado solo borralo, es un comentario, no afectará en nada
  * @type {PortafolioData[]}
  */
 export const portafolioData = [
 	{
-		imgSrc: 'https://dt2sdf0db8zob.cloudfront.net/wp-content/uploads/2021/06/copy-of-the-6-best-restaurant-website-templates-1.png',
+		imgSrc: '../../../public/img/cook.png',
 		title: 'Restaurant',
-		skills: ['React', 'StyledComponents'],
+		skills: ['React', 'Tailwind'],
 		descripcion:
-			'Laboris ex laboris mollit esse fugiat aute cillum nostrud enim dolor sit. Reprehenderit et non nulla irure aute nostrud commodo aute.',
-		demoURL: '',
-		repoURL: '',
+			"Welcome to our online gourmet marketplace featuring a wide selection of the finest foods and spices from around the world. Explore our curated collection of culinary delights and discover the flavors that will elevate your cooking.",
+		demoURL: 'https://landing-food.netlify.app/',
+		repoURL: 'https://github.com/juan1779/Landing-food',
+		caseURL: '',
 		anim: 'fade-right',
 	},
 	{
-		imgSrc: 'https://fiverr-res.cloudinary.com/images/q_auto,f_auto/gigs/299377097/original/7eb7dcebe244fcf5ad75d92b0969fc116946bd57/create-professional-amd-responsive-wordpress-website.jpg',
-		title: 'E-commerce',
-		skills: ['JavaScript', 'Bootstrap'],
+		imgSrc: '../../../public/img/secure.png',
+		title: 'Landing Page',
+		skills: ['React', 'Tailwind'],
 		descripcion:
-			' Laboris ex laboris mollit esse fugiat aute cillum nostrud enim dolor sit. Reprehenderit et non nulla irure aute nostrud commodo aute.',
-		demoURL: '',
-		repoURL: '',
+			"Unlock the power of blockchain technology with our comprehensive suite of services. Whether you're an enterprise looking to streamline operations or a startup seeking to disrupt your industry, our team of blockchain experts can help you leverage this transformative innovation. ",
+		demoURL: 'https://landing-page-block.netlify.app/',
+		repoURL: 'https://github.com/juan1779/LandingPage',
+		caseURL: '',
 		anim: 'fade-up',
 		averageBrightness: 0.1,
 	},
@@ -42,9 +45,10 @@ export const portafolioData = [
 		title: 'Coffe Store',
 		skills: ['React', 'CSS'],
 		descripcion:
-			'Laboris ex laboris mollit esse fugiat aute cillum nostrud enim dolor sit. Reprehenderit et non nulla irure aute nostrud commodo aute.Laboris ex laboris mollit esse fugiat aute cillum nostrud enim dolor sit. Reprehenderit et non nulla irure aute nostrud commodo aute.Laboris ex laboris mollit esse fugiat aute cillum nostrud enim dolor sit. Reprehenderit et non nulla irure aute nostrud commodo aute.',
-		demoURL: '',
-		repoURL: '',
+			"This project is a login simulation created using React, Redux, and localStorage. The application allows users to sign in and out, with their login status and user data stored in the browser's localStorage",
+		demoURL: 'https://login-react-redux-localstorage.netlify.app/',
+		repoURL: 'https://github.com/juan1779/Login-React',
+		caseURL: '',
 		anim: 'fade-left',
 	},
 	{
@@ -55,6 +59,7 @@ export const portafolioData = [
 			'Laboris ex laboris mollit esse fugiat aute cillum nostrud enim dolor sit. Reprehenderit et non nulla irure aute nostrud commodo aute.Laboris ex laboris mollit esse fugiat aute cillum nostrud enim dolor sit. Reprehenderit et non nulla irure aute nostrud commodo aute.Laboris ex laboris mollit esse fugiat aute cillum nostrud enim dolor sit. Reprehenderit et non nulla irure aute nostrud commodo aute.',
 		demoURL: '',
 		repoURL: '',
+		caseURL: '',
 		anim: 'fade-left',
 	},
 	{
@@ -65,6 +70,7 @@ export const portafolioData = [
 			'Laboris ex laboris mollit esse fugiat aute cillum nostrud enim dolor sit. Reprehenderit et non nulla irure aute nostrud commodo aute.Laboris ex laboris mollit esse fugiat aute cillum nostrud enim dolor sit. Reprehenderit et non nulla irure aute nostrud commodo aute.Laboris ex laboris mollit esse fugiat aute cillum nostrud enim dolor sit. Reprehenderit et non nulla irure aute nostrud commodo aute.',
 		demoURL: '',
 		repoURL: '',
+		caseURL: '',
 		anim: 'fade-left',
 	},
 	{
@@ -75,6 +81,7 @@ export const portafolioData = [
 			'Laboris ex laboris mollit esse fugiat aute cillum nostrud enim dolor sit. Reprehenderit et non nulla irure aute nostrud commodo aute.',
 		demoURL: '',
 		repoURL: '',
+		caseURL: '',
 		anim: 'fade-right',
 	}
 ];
